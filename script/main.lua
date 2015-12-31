c3951e818d4f7f6d99f780c031de56d7fc997917
Zwagoth <zabb65@gmail.com>
2015-12-31 03:42:04 -0500

    Better logging for staff call confirmation.

--- script/main.lua
+++ script/main.lua
@@ -1401,9 +1401,9 @@ function (con, args)
 		if channel_override == false and u.isGlobMod(con) ~= true and u.isAdmin(con) ~= true then
 			return const.FERR_NOT_OP
 		end
-		s.logAction(con, "SFC", args)
+		local lsfc = {action="confirm", moderator=u.getName(con), character=call.character, timestamp=call.timestamp, tab=s.escapeHTML(call.tab), logid=call.logid}
+		s.logAction(con, "SFC", lsfc)
 		s.removeStaffCall(args.callid)
-		local lsfc = {action="confirm", moderator=u.getName(con), character=call.character, timestamp=call.timestamp, tab=s.escapeHTML(call.tab)}
 		s.broadcastOps("SFC", lsfc)
 		if chanfound == true then
 			broadcastChannelOps("SFC", lsfc, chan)
